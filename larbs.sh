1746c029ac6fbf8c6d0b3f4b3b7fe18005c4808a
LORDBABUINO <jorge.x7@gmail.com>
2019-02-19 22:28:11 +0000

    Makes git pull submodules

--- larbs.sh
+++ larbs.sh
@@ -130,7 +130,7 @@ putgitrepo() { # Downlods a gitrepo $1 and places the files in $2 only overwriti
 	dir=$(mktemp -d)
 	[ ! -d "$2" ] && mkdir -p "$2" && chown -R "$name:wheel" "$2"
 	chown -R "$name:wheel" "$dir"
-	sudo -u "$name" git clone --depth 1 "$1" "$dir/gitrepo" >/dev/null 2>&1 &&
+ 	sudo -u "$name" git clone --depth 1 --recurse-submodules "$1" "$dir/gitrepo" >/dev/null 2>&1 &&
 	sudo -u "$name" cp -rfT "$dir/gitrepo" "$2"
 	}
 
